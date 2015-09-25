d290f474ec8b8809ceb4913fb0c1d06ead38db83
Leandro Guillen <leandroguillen@gmail.com>
2015-09-25 12:31:01 +0200

    Excercise 2

--- exercises/ex02.go
+++ exercises/ex02.go
@@ -4,46 +4,97 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"io/ioutil"
-
 	"fmt"
+	"io/ioutil"
 	"net/http"
-
-	// "os"
+	"strconv"
 )
 
 type UpdateContextRequest struct {
-	ContextElements []ContextElement `json: "contextElements"`
-	UpdateAction    string           `json: "updateAction"`
+	ContextElements []ContextElement `json:"contextElements"`
+	UpdateAction    string           `json:"updateAction"`
 }
 
 type ContextElement struct {
-	Id         string      `json: "id"`
-	IsPattern  string      `json: "isPattern"`
-	Type       string      `json: "type"`
-	Attributes []Attribute `json: "attributes"`
+	Id         string      `json:"id"`
+	IsPattern  string      `json:"isPattern"`
+	Type       string      `json:"type"`
+	Attributes []Attribute `json:"attributes"`
 }
 
 type Attribute struct {
-	Name  string `json: "id"`
-	Type  string `json: "type"`
-	Value string `json: "value"`
+	Name  string `json:"name"`
+	Type  string `json:"type"`
+	Value string `json:"value"`
+}
+
+type Room struct {
+	Name        string
+	Temperature float64 `json:"temperature"`
+	Presence    bool    `json:"presence"`
+	Status      string  `json:"status"`
+}
+
+type Door struct {
+	Name   string
+	Locked bool `json:"locked"`
+	Closed bool `json:"closed"`
+}
+
+type NGSI interface {
+	ToNGSI() ContextElement
+}
+
+func (r *Room) ToNGSI() ContextElement {
+	return ContextElement{
+		Id:        r.Name,
+		IsPattern: "false",
+		Type:      "Room",
+		Attributes: []Attribute{
+			Attribute{"temperature", "float", strconv.FormatFloat(r.Temperature, 'f', -1, 32)},
+			Attribute{"presence", "boolean", strconv.FormatBool(r.Presence)},
+			Attribute{"status", "string", r.Status},
+		},
+	}
+}
+
+func (d *Door) ToNGSI() ContextElement {
+	return ContextElement{
+		Id:        d.Name,
+		IsPattern: "false",
+		Type:      "Door",
+		Attributes: []Attribute{
+			Attribute{"locked", "boolean", strconv.FormatBool(d.Locked)},
+			Attribute{"closed", "boolean", strconv.FormatBool(d.Closed)},
+		},
+	}
 }
 
 func main() {
 
-	ucr := &UpdateContextRequest{
-		[]ContextElement{
-			ContextElement{"id1", "false", "aType", []Attribute{Attribute{"name1", "type1", "val1"}}},
-			ContextElement{"id2", "false", "aType", []Attribute{Attribute{"name2", "type2", "val2"}}},
-		},
-		"APPEND",
+	room1 := Room{"Bedroom1", 25.5, false, "OK"}
+	room2 := Room{"Bedroom2", 26.0, true, "Needs cleaning"}
+	room3 := Room{"Kitchen", 28.9, true, "OK"}
+	door1 := Door{"Frondoor", false, true}
+	door2 := Door{"Backdoor", false, false}
+
+	// Create array of context elements
+	entities := []ContextElement{
+		room1.ToNGSI(),
+		room2.ToNGSI(),
+		room3.ToNGSI(),
+		door1.ToNGSI(),
+		door2.ToNGSI(),
 	}
 
+	ucr := &UpdateContextRequest{entities, "APPEND"}
+
 	ucr_json, _ := json.Marshal(ucr)
+	fmt.Println(string(ucr_json))
+
 	url := "http://localhost:1026/v1/updateContext"
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(ucr_json))
-	req.Header.Set("X-Custom-Header", "myvalue")
+	req.Header.Set("Accept", "application/json")
 	req.Header.Set("Content-Type", "application/json")
 
 	client := &http.Client{}
