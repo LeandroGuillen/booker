395b883e66fc47cfdb70ebcabe67b1c03800e6fd
Leandro Guillen <leandroguillen@gmail.com>
2016-01-18 11:33:13 +0100

    Booker updated to v2

--- booker/booker.js
+++ booker/booker.js
@@ -3,67 +3,105 @@
 // Examples:
 // - Get Orion Context Broker version:
 //		./booker.js
+// - Create a table named 'table1':
+//		./booked.js --table table1 --create
 // - Get table1 booking status:
 //		./booker.js --table table1
 // - Set table1 booking status:
 //		./booker.js --table table1 --booked true
+// - Delete a table named 'table1':
+//		./booked.js --table table1 --delete
  
 var request = require('request');
 var argv = require('optimist').argv;
  
-var urlBase = "http://leandroguillen.com:1026"
+var urlBase = "http://localhost:1026"
  
-if (argv.table && argv.booked)
+// Changing booking status
+if (argv.table && (argv.booked || argv.book || argv.unbook))
 {
-	payload={
-		    "contextElements": [
-		        {
-		          "type": "Table",
-		          "isPattern": "false",
-		          "id": argv.table,
-		          "attributes": [
-		            {
-		              "name": "booked",
-		              "type": "boolean",
-		              "value": argv.booked
-		            }
-		          ]
-		        }
-		    ],
-		    "updateAction": "UPDATE"
+	if (argv.book) {
+		payload={
+	    	"booked": "true"
 		}
-	
-	request.post(
-	    urlBase+'/v1/updateContext',
+		newStatus = "true"
+	} else if (argv.unbook) {
+		payload={
+	    	"booked": "false"
+		}
+		newStatus = "false"
+	} else {
+		
+		newStatus = argv.booked
+	}
+
+	payload={
+		   "booked": argv.booked
+	}
+		 
+	request.patch(
+	    urlBase+'/v2/entities/' + argv.table,
 	    { json: payload },
 	    function (error, response, body) {
-	        if (!error && response.statusCode == 200) {
-	        	if (body.statusCode)
-	        	{
-	        		console.log(body.statusCode.reasonPhrase)
-	        	}
-	        	else
-	        	{
-		        	if (body.contextResponses[0].statusCode.code == "200")
-		        	{
-		        		console.log('Table "'+argv.table+'": booked state is set to "'+argv.booked+'"')
-		            	//console.log(JSON.stringify(body, null, 2))
-		        	}
-		        	else
-		        	{
-		        		console.log(body.contextResponses[0].statusCode.reasonPhrase)
-		        	}		
-	        	}
+	        if (!error && response.statusCode == 204) {
+       			console.log('Table "'+argv.table+'": booked state is set to "'+newStatus+'"')
+	        } else {
+	        	console.log("Maybe the table does not exist?")
+	        }
+	    }
+	);
+}
+
+// Creating a new table
+else if (argv.table && argv.create)
+{
+	payload={
+	    "id": argv.table,
+	    "type": "Table",
+	    "isPattern": "false",
+	    "booked": "false"
+	}
+	
+	request.post(
+	    urlBase+'/v2/entities',
+	    { json: payload },
+	    function (error, response, body) {
+	        if (!error && response.statusCode == 201) {
+	        	console.log('Table "'+argv.table+'" was created.')
 	        } else {
 	        	console.log(error)
 	        }
 	    }
 	);
 }
+
+// Removing a table
+else if (argv.table && argv.del)
+{
+	var options = {
+	    url: urlBase+'/v2/entities/' + argv.table,
+	    headers: {
+	        'Accept': 'application/json'
+	    }
+	};
+
+	request.del(
+	    options,
+	    function (error, response, body) {
+            if (!error && response.statusCode == 204) {
+	        	console.log('Table "'+argv.table+'" was deleted.')
+	        } else {
+	        	console.log("Maybe the table does not exist already?")
+	        }
+	    }
+	);
+}
+
+// Querying booking status
 else if (argv.table)
 {
 	var options = {
-	    url: urlBase+'/v1/contextEntities/' + argv.table + '/attributes/booked',
+	    url: urlBase+'/v2/entities/' + argv.table,
 	    headers: {
 	        'Accept': 'application/json'
 	    }
@@ -71,31 +109,32 @@ else if (argv.table)
 	request.get(
 	    options,
 	    function (error, response, body) {
-	        if (!error && response.statusCode == 200) {
+	        if (!error && response.statusCode == 200)
+	        {
 	        	var json = JSON.parse(body)
-	            if (json.statusCode && !json.attributes)
-	        	{
-	        		console.log(json.statusCode)
-	        	}
-	        	else
-	        	{
-		            var isBooked = json.attributes[0].value
-		            if (isBooked === 'true')
-		            {
-		            	console.log('Table "'+argv.table+'" is booked.')
-		            }
-		            else
-		            {
-		            	console.log('Table "'+argv.table+'" is free.')
-		            }
+	            var isBooked = json.booked
+	            if (isBooked === "true")
+	            {
+	            	console.log('Table "'+argv.table+'" is booked.')
 	            }
-	        } else {
+	            else
+	            {
+	            	console.log('Table "'+argv.table+'" is free.')
+	            }
+	        } else if (!error && response.statusCode == 404)
+	        {
+	        	console.log("The table does not exist.")
+	        }
+	        else {
 	        	console.log('error:' + error)
 	        	console.log('statusCode:' + response.statusCode)
+	        	console.log("Maybe the table does not exist?")
 	        }
 	    }
 	);
 }
+
+// By default just print Orion version
 else
 {
 	console.log('No parameters found')
